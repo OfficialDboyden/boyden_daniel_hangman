af88d84f00d4f6c516cf84ecd47bd30dc044208d
Helen Unger <imac@imacs-iMac.local>
2018-03-02 18:41:04 -0500

    fixed the filesand added css to make it look better

--- js/main.js
+++ js/main.js
@@ -10,59 +10,10 @@ let currentWord = null,
   guessBox = document.querySelector('.letter-guess'),
   wrongGuesses = 0,
   resetScreen = document.querySelector('.reset-screen'),
-  resetButton = resetScreen.querySelector('button'),
+  resetButton = resetScreen.querySelector ('.reset-button'),
   wrongLetters = document.querySelector('.wrong-letters'),
   wrongLetterArray = [];
 
-
-function showResetScreen()
-{
-resetScreen.classList.add('showBodyPart');
-}
-
-function resetGame() {
-  wrongGuesses = 0;
-  let gamePieces = array.From(document.querySelectorAll('showBodyPart'));
-  gamePieces.forEach(piece => piece.classList.remove('showBodyPart'));
-  init();
-}
-
-function makeGuess() {
- console.log(this.value)
-
-//if no letter
-  if (this.value == "" || this.value.length < 1) {
-    return;
-  }
-  if (currentWord.indexOf(this.value) < 0) {
-    wrongLetterArray.push(this.value);
-    wrongLetters.textContent = wrongLetterArray.join(" ");
-
-    document.querySelector(`.wrong${wrongGuesses}`).classList.add('showBodyPart');
-
-    if (wrongGuesses >= 5)
-    {
-      console.log('you lose, loser');
-    }
-    else
-    {
-      document.querySelector(`.wrong${wrongGuesses}`).classList.add('showBodyPart');
-    }
-
-    wrongGuesses++; //adds one to whatever that value was already
-  }
-
-  else {
-    // checking for the win path
-  }
-}
-
-//conditional statement
-//inside the round brackets, check for condition
-//'index of' means find the position of where the character is inside of the string, if its not there the output is -1 which is why it has to be above 0 to be a right answer
-
-
-guessBox.addEventListener('keyup', makeGuess);
 
 //when game starts or REstarts, o these things
 function init()
@@ -77,8 +28,71 @@ function init()
   wordHint.textContent = currentWord.split("").map(letter => letter = "__").join(" ");
 
   console.log(`guess this word: ${currentWord}. Its at ${words.indexOf(currentWord)}`);
-
 }
+
+function showResetScreen()
+{
+  resetScreen.classList.remove('reset-screen');
+}
+
+function resetGame()
+{
+  wrongGuesses = 0;
+  wrongLetterArray = [];
+  wrongLetters.textContent = "";
+  let gamePieces = Array.from(document.querySelectorAll('.showBodyPart'));
+  gamePieces.forEach(piece => piece.classList.remove('showBodyPart'));
+  resetScreen.classList.add('reset-screen');
+  init();
+}
+
+//functions go in the middle (logic)
+function makeGuess()
+{
+    console.log(this.value);
+
+    //if no letter
+    if (this.value == "" || this.value.length < 1)
+    {
+     return;
+    }
+
+    //conditional statement
+    //inside the round brackets, check for condition
+    //'index of' means find the position of where the character is inside of the string, if its not there the output is -1 which is why it has to be above 0 to be a right answer
+    if (currentWord.indexOf(this.value) < 0)
+    {
+      wrongLetterArray.push(this.value);
+      wrongLetters.textContent = wrongLetterArray.join(" ");
+
+    document.querySelector(`.wrong${wrongGuesses}`).classList.add('showBodyPart');
+    document.getElementById('myLetter').value = "";
+
+        if (wrongGuesses >= 5)
+        {
+          console.log('you lose, loser');
+          showResetScreen();
+        }
+        else
+        {
+        document.querySelector(`.wrong${wrongGuesses}`).classList.add('showBodyPart');
+        }
+
+    wrongGuesses++; //adds one to whatever that value was already
+    }
+    else {
+      // checking for the win path
+    }
+}
+
+
+
+
+
+//event handling goes at the bottom
+//goes to guess box, listens for event keyup(what are you listening for) makeGuess (what do you want top happen)
+guessBox.addEventListener('keyup', makeGuess);
+resetButton.addEventListener("click", resetGame, true);
 
 
 init();
