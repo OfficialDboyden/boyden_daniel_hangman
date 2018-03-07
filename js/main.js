7d1d9912e3ae9c467553ddb9ca58083c972a58cf
Unger <h_unger@tss-hr420-dm26.fc.ca>
2018-03-07 16:48:38 -0500

    we are done

--- js/main.js
+++ js/main.js
@@ -9,10 +9,12 @@ let currentWord = null,
   wordHint = document.querySelector('.current-word'),
   guessBox = document.querySelector('.letter-guess'),
   wrongGuesses = 0,
+  correctGuesses = 0,
   resetScreen = document.querySelector('.reset-screen'),
   resetButton = resetScreen.querySelector ('.reset-button'),
   wrongLetters = document.querySelector('.wrong-letters'),
-  wrongLetterArray = [];
+  wrongLetterArray = [],
+  correctLetterArray = [];
 
 
 //when game starts or REstarts, o these things
@@ -30,14 +32,18 @@ function init()
   console.log(`guess this word: ${currentWord}. Its at ${words.indexOf(currentWord)}`);
 }
 
-function showResetScreen()
+function showResetScreen(message, source)
 {
-  resetScreen.classList.remove('reset-screen');
+  resetScreen.classList.add('showBodyPart');
+  //grab header tag and change the content
+  resetScreen.querySelector('h3').textContent = message;
+  resetScreen.querySelector('img').src = source;
 }
 
 function resetGame()
 {
   wrongGuesses = 0;
+  correctGuesses = 0;
   wrongLetterArray = [];
   wrongLetters.textContent = "";
   let gamePieces = Array.from(document.querySelectorAll('.showBodyPart'));
@@ -56,14 +62,33 @@ function makeGuess()
     {
      return;
     }
+    let currentGuess = this.value;
+
+    //if the current guess is ==
+    if (wrongLetterArray.indexOf(this.value) >= 0)
+    {
+      window.alert("The letter was already guessed, it was wrong.");
+      document.getElementById('myLetter').value = "";
+      return;
+    }
+
+    if (correctLetterArray.indexOf(this.value) >= 0)
+    {
+      window.alert("The letter was already guessed, it was right.");
+      document.getElementById('myLetter').value = "";
+      return;
+    }
+
+
 
     //conditional statement
     //inside the round brackets, check for condition
     //'index of' means find the position of where the character is inside of the string, if its not there the output is -1 which is why it has to be above 0 to be a right answer
-    if (currentWord.indexOf(this.value) < 0)
+    if (currentWord.indexOf(this.value) < 0) //the losing path
     {
       wrongLetterArray.push(this.value);
       wrongLetters.textContent = wrongLetterArray.join(" ");
+
 
     document.querySelector(`.wrong${wrongGuesses}`).classList.add('showBodyPart');
     document.getElementById('myLetter').value = "";
@@ -71,7 +96,7 @@ function makeGuess()
         if (wrongGuesses >= 5)
         {
           console.log('you lose, loser');
-          showResetScreen();
+          showResetScreen("You lose, Loser! Play again?","images/losing.jpg");
         }
         else
         {
@@ -80,8 +105,32 @@ function makeGuess()
 
     wrongGuesses++; //adds one to whatever that value was already
     }
-    else {
-      // checking for the win path
+    else // checking for the win path
+    {
+      correctLetterArray.push(this.value);
+
+      //split the current word into an array and check letter by letter
+      var matchAgainst = currentWord.split('');
+      var hintString = wordHint.textContent.split(' '); // split on the characters, not the underscores
+      //check each letter in the current word
+      matchAgainst.forEach((letter,index) => {
+        if (letter == currentGuess)
+        {
+          hintString[index] = currentGuess;
+
+          correctGuesses++
+
+        }
+      });
+
+      document.getElementById('myLetter').value = "";
+      wordHint.textContent = "";
+      wordHint.textContent = hintString.join(" ");
+
+      if (correctGuesses == currentWord.length) {
+        showResetScreen("You won! Play again?","images/winning.jpg");
+      }
+
     }
 }
 
